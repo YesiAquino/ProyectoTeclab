c29fb8a49be36a27a675f4fb54136ac94fd656ae
YesiAquino <yesiaquino95@gmail.com>
2024-08-04 16:33:52 -0300

    form

--- Productos/formulario.php
+++ Productos/formulario.php
@@ -3,19 +3,19 @@ require '../conexion.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/ProyectoTeclab/Vista/css/formulario.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Inventario</title>
-</head>
-<body>
-    <!-- Encabezado -->
-    <div class="encabezado">
-        <img src="http://localhost/ProyectoTeclab/Vista/img/logo.png" alt="">
-    </div>
-    <!-- Inicio -->
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="http://localhost/ProyectoTeclab/Vista/css/formulario.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Inventario</title>
+    </head>
+    <body>
+        <!-- Encabezado -->
+        <div class="encabezado">
+            <img src="http://localhost/ProyectoTeclab/Vista/img/logo.png" alt="">
+        </div>
+        <!-- Inicio -->
         <div class="contenedor-formulario"> 
             <form action="http://localhost/ProyectoTeclab/Productos/ABM/altaProductos.php" method="POST" id="form-productos">
                 <h2>Agregar Producto</h2>           
@@ -27,8 +27,8 @@ require '../conexion.php';
                     <input id="precio-producto" type="text" name="precioProducto">
                 <label for="cantidad-producto">Cantidad:</label>
                     <input id="cantidad-producto" type="text" name="cantidadProducto">
-                <label for="file_producto"><i class="fa-regular fa-file-excel"></i></label>
-                    <input id="file_producto" type="file" name="archivoP">                
+                <!-- <label for="file_producto"><i class="fa-regular fa-file-excel"></i></label>
+                    <input id="file_producto" type="file" name="archivoP">                 -->
                 <label for="proveedor-producto">Proveedor:</label>
                     <select name="proveedorProducto" id="proveedorProducto">
                         <option selected disabled>--Seleccionar categoria--</option>
@@ -37,7 +37,7 @@ require '../conexion.php';
                                 $resultado = pg_query($conexion, $sql);
 
                                 while ($obj = pg_fetch_assoc($resultado)) {
-                                    echo "<option value='" . $obj['id_prov'] . "'>" . $obj['nombre_prov'] . "</option>";
+                                        echo "<option value='" . $obj['id_prov'] . "'>" . $obj['nombre_prov'] . "</option>";
                                 }
                             ?>
                     </select>                
@@ -46,6 +46,6 @@ require '../conexion.php';
                     <button class="reset" type="reset" name="Cancelar"><a href="http://localhost/ProyectoTeclab/Productos/productos.php">CANCELAR</a></button>
                 </div>          
             </form>
-        </div>
+        </div>      
     </body>
 </html>
