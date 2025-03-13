b316d6078b3cce56c24114ef551c02ae35212c29
YesiAquino <yesiaquino95@gmail.com>
2025-03-12 21:32:32 -0300

    eliminar_prod

--- Productos/ABM/eliminarDato.php
+++ Productos/ABM/eliminarDato.php
@@ -2,7 +2,8 @@
     include ("../../conexion.php");
 
     $Id = $_REQUEST['Id'];
-    $sql = "DELETE FROM productos WHERE id_prod = '$Id'";
+    $sql = "DELETE FROM entradas WHERE id_prodentrada = '$Id';
+            DELETE FROM productos WHERE id_prod = '$Id';";           
 
     if ($obj = pg_query($conexion, $sql)) {
         header("location:../productos.php");
